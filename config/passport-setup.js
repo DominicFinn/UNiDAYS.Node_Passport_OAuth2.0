42a7eefa42dae7e8ff6262984a3045fc2c0c153d
DomFinn <dom.finn@gmail.com>
2021-11-23 00:22:57 +0000

    encodes subs from UNiDAYS Connect. A Sub may contain slashes that causes issues in many database (firebase for example)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,12 +3,13 @@ const OAuth2Strategy = require('passport-oauth2').Strategy;
 const keys = require('./keys');
 const User = require('../models/user-model');
 const fetch = require("node-fetch");
+const { encode } = require('js-base64');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser(async (id, done) => {    
+passport.deserializeUser(async (id, done) => {
     const user = await User.findById(id);
     done(null, user);
 });
@@ -19,34 +20,38 @@ passport.use(new OAuth2Strategy({
     clientID: keys.unidays.clientID,
     clientSecret: keys.unidays.clientSecret,
     callbackURL: "http://localhost:8080/auth/unidays-callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {      
-    fetch('https://account.myunidays.com/oauth/userinfo', {
-        method: 'GET', 
-        headers: {
-          'Authorization': 'Bearer ' + accessToken
-        },
-    }).then(res => res.json())
-    .then(async (json) => {
-        if(!json) {
-            return;
-        }
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(accessToken)
 
-        const user = {
-            id: json.sub,
-            email: json.email,
-            given_name: json.given_name,
-            family_name: json.family_name,
-            country_of_study: json.country_of_study,
-            verification_status: {
-                verified: json.verification_status.verified,
-                user_type: json.verification_status.user_type
-            }
-        };
-        
-        await User.findOrCreate(user, function (err, user) {
-            return cb(err, user);
-        });
-    });
-  }
+        fetch('https://account.myunidays.com/oauth/userinfo', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+        }).then(res => res.json())
+            .then(async (json) => {
+                if (!json) {
+                    return;
+                }
+
+                const user = {
+                    id: encode(json.sub),
+                    email: json.email,
+                    given_name: json.given_name,
+                    family_name: json.family_name,
+                    country_of_study: json.country_of_study,
+                    verification_status: {
+                        verified: json.verification_status.verified,
+                        user_type: json.verification_status.user_type
+                    }
+                };
+
+                console.log(JSON.stringify(user))
+
+                await User.findOrCreate(user, function (err, user) {
+                    return cb(err, user);
+                });
+            });
+    }
 ));
